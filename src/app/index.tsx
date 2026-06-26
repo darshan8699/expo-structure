315ee686a4bf07c7cd962e0d72b6ae0727d368e1
Darshan Rana <darshan.rana@zyrous.com>
2026-06-26 15:01:32 +0530

    feat: testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Hello World</Text>
+            <Text style={styles.text}>Hello World. This is Expo structure</Text>
         </View>
     )
 }
